ba496e62ca8e0e21ef6fc4da18df6ba294a7131c
Beverly Li <bel058@ucsd.edu>
2020-01-23 10:27:35 -0800

    hello world

--- hello/app.js
+++ hello/app.js
@@ -3,6 +3,11 @@
  * Module dependencies.
  */
 
+//var favicon = require('serve-favicon');
+//var logger = require('morgan');
+//var methodOverride = require('method-override');
+//var cookieParser = require('cookie-parser');
+//var session = require('express-session');
 var express = require('express');
 var http = require('http');
 var path = require('path');
@@ -19,10 +24,16 @@ app.engine('handlebars', handlebars());
 app.set('view engine', 'handlebars');
 app.use(express.favicon());
 app.use(express.logger('dev'));
+//app.use(favicon());
+//app.use(express.static(path.join(__dirname, 'node_modules', 'favicon.ico')));
+//app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded());
+//app.use(methodOverride());
 app.use(express.methodOverride());
+//app.use(cookieParser('Intro HCI secret key'));
 app.use(express.cookieParser('Intro HCI secret key'));
+//app.use(session());
 app.use(express.session());
 app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
